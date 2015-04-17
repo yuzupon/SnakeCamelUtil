89be172960a64aec96d7d5368364476500040b2f
aoiaoiaoi <tsuda.soft.2015@gmail.com>
2015-04-17 18:42:08 +0900

    課題3-2のプロジェクト。エラーを修正してください

---
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -0,0 +1,46 @@
+package snakecamel;
+
+public class SnakeCamelUtil {
+
+	public static String snakeToCamelcase(String snake_case) {
+		String[] words = snake_case.split("_");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < words.length - 1; i++) {
+			sb.append(capitalize(words[i]));
+		}
+		return new String(sb);
+	}
+	 
+	public static String camelToSnakecase(String camelcase) {
+		StringBuilder sb = new StringBuilder();
+		int j = 0;
+		for (int i = 0; i < camelcase.length(); i++) {
+			char c = camelcase.charAt(i);
+			if (Character.isUpperCase(c)) {
+				sb.append(camelcase.substring(j, i));
+				if (sb.length() > 0) {
+					sb.append("_");
+				}
+				sb.append(Character.toLowerCase(c));
+				j = i;
+			}
+		}
+		sb.append(camelcase.substring(j));
+		return new String(sb);
+	}
+	
+	static String capitalize(String s) {
+		char first = s.charAt(0);
+		char upperFirst = Character.toUpperCase(first);
+		String rest = s.substring(1);
+		return upperFirst + rest;
+	}
+
+	static String uncapitalize(String s) {
+		char first = s.charAt(0);
+		char lowerFirst = Character.toLowerCase(first);
+		String rest = s.substring(1);
+		return lowerFirst + rest;
+	}
+	
+}
