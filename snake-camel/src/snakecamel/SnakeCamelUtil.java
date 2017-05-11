10d8320ebe1d638272d7cc897297fcf86c638e57
yuzuki <g15906fy@s30432.tsuda.ac.jp>
2017-05-11 17:53:49 +0900

    エラー修正しました

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,8 +5,10 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length ; i++) {
+			if(words[i].length()!=0){
 			sb.append(capitalize(words[i]));
+			}
 		}
 		return new String(sb);
 	}
@@ -22,7 +24,7 @@ public class SnakeCamelUtil {
 					sb.append("_");
 				}
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
